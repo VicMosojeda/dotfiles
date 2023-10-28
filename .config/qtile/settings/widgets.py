47100ce1a75b7d0a228d10d18e8c3bed64df3ac6
Metadeth <victormosquedaojeda66@gmail.com>
2023-10-27 23:24:36 -0600

    qtile conf

--- .config/qtile/settings/widgets.py
+++
@@ -1,9 +0,0 @@
-from libqtile import widget
-
-widget_defaults = dict(
-    # font="Hack Nerd Font",
-    font="novamono for powerline",
-    fontsize=15,
-    padding=1,
-)
-extension_defaults = widget_defaults.copy()
