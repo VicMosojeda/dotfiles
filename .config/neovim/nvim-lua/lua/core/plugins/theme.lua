a5aa7b5f6436a69c96bb5acd80d9be8e65aa542e
vickmetadeth <121125358+VicMosojeda@users.noreply.github.com>
2024-10-23 23:34:00 -0600

    Update theme.lua

--- .config/neovim/nvim-lua/lua/core/plugins/theme.lua
+++ .config/neovim/nvim-lua/lua/core/plugins/theme.lua
@@ -1,5 +1,5 @@
 vim.cmd[[ set background=dark ]]
-vim.cmd[[ colorscheme cyberdream ]]
+vim.cmd[[ colorscheme onedark ]]
 
 vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
